bb8c67e2594d9cae90931c46cab242c1d50e168e
ckruger097 <ckruger4@gmu.edu>
2024-05-03 22:04:25 -0500

    new ignore

---
+++ app/components/WalletContextProvider.tsx
@@ -0,0 +1,29 @@
+import { FC, ReactNode, useMemo } from "react"
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react"
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
+import * as web3 from "@solana/web3.js"
+import * as walletAdapterWallets from "@solana/wallet-adapter-wallets"
+require("@solana/wallet-adapter-react-ui/styles.css")
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const endpoint = new web3.Connection("http://127.0.0.1:8899")
+  const wallets = useMemo(() => {
+    return [
+      new walletAdapterWallets.PhantomWalletAdapter(),
+      new walletAdapterWallets.SolflareWalletAdapter(),
+    ]
+  }, [])
+
+  return (
+    <ConnectionProvider endpoint={endpoint.rpcEndpoint}>
+      <WalletProvider wallets={wallets} autoConnect={true}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
+
+export default WalletContextProvider
---
+++ tests/anchor_counter.ts
@@ -0,0 +1,35 @@
+import * as anchor from "@coral-xyz/anchor"
+import { Program } from "@coral-xyz/anchor"
+import { expect } from "chai"
+import { AnchorCounter } from "../target/types/anchor_counter"
+
+describe("anchor-counter", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
+
+  const program = anchor.workspace.AnchorCounter as Program<AnchorCounter>
+
+  const counter = anchor.web3.Keypair.generate()
+
+  it("Is initialized!", async () => {
+    const tx = await program.methods
+        .initialize()
+        .accounts({ counter: counter.publicKey })
+        .signers([counter])
+        .rpc()
+
+    const account = await program.account.counter.fetch(counter.publicKey)
+    expect(account.count.toNumber()).to.equal(0)
+  })
+
+  it("Incremented the count", async () => {
+    const tx = await program.methods
+        .increment()
+        .accounts({ counter: counter.publicKey, user: provider.wallet.publicKey })
+        .rpc()
+
+    const account = await program.account.counter.fetch(counter.publicKey)
+    expect(account.count.toNumber()).to.equal(1)
+  })
+})
